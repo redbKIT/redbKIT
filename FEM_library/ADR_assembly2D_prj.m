89632deb95dbb43decb4a6ce7ee73aefbc7f4821
Federico Negri <federico.negri@epfl.ch>
2016-03-22 17:10:36 +0100

    New C_OMP 2D ADR assembler. Much faster than existing mex assembler

--- FEM_library/ADR_assembly2D_prj.m
+++ FEM_library/ADR_assembly2D_prj.m
@@ -21,22 +21,22 @@ ARGS{1} = cell(19,1);
 ARGS{1}{1} = coder.typeof('X',[1 50],[0 1]);
 ARGS{1}{2} = coder.typeof(0,[1 2],[0 1]);
 ARGS{1}{3} = coder.typeof(0);
-ARGS{1}{4} = coder.typeof(0,[20 3000000],[1 1]);
+ARGS{1}{4} = coder.typeof(0,[20 inf],[1 1]);
 ARGS{1}{5} = coder.typeof(0);
-ARGS{1}{6} = coder.typeof(0,[3000000    60],[1 1]);
-ARGS{1}{7} = coder.typeof(0,[3000000    60],[1 1]);
-ARGS{1}{8} = coder.typeof(0,[3000000    60],[1 1]);
-ARGS{1}{9} = coder.typeof(0,[3000000    60],[1 1]);
-ARGS{1}{10} = coder.typeof(0,[3000000    60],[1 1]);
+ARGS{1}{6} = coder.typeof(0,[inf    60],[1 1]);
+ARGS{1}{7} = coder.typeof(0,[inf    60],[1 1]);
+ARGS{1}{8} = coder.typeof(0,[inf    60],[1 1]);
+ARGS{1}{9} = coder.typeof(0,[inf    60],[1 1]);
+ARGS{1}{10} = coder.typeof(0,[inf    60],[1 1]);
 ARGS{1}{11} = coder.typeof(0,[1 60],[0 1]);
-ARGS{1}{12} = coder.typeof(0,[1 3000000],[0 1]);
-ARGS{1}{13} = coder.typeof(0,[1 3000000],[0 1]);
-ARGS{1}{14} = coder.typeof(0,[1 3000000],[0 1]);
-ARGS{1}{15} = coder.typeof(0,[1 3000000],[0 1]);
+ARGS{1}{12} = coder.typeof(0,[1 inf],[0 1]);
+ARGS{1}{13} = coder.typeof(0,[1 inf],[0 1]);
+ARGS{1}{14} = coder.typeof(0,[1 inf],[0 1]);
+ARGS{1}{15} = coder.typeof(0,[1 inf],[0 1]);
 ARGS{1}{16} = coder.typeof(0,[20 60],[1 1]);
 ARGS{1}{17} = coder.typeof(0,[20 60],[1 1]);
 ARGS{1}{18} = coder.typeof(0,[20 60],[1 1]);
-ARGS{1}{19} = coder.typeof(0,[1 3000000],[0 1]);
+ARGS{1}{19} = coder.typeof(0,[1 inf],[0 1]);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Invoke MATLAB Coder.
