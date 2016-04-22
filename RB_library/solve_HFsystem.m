d063ffb297ebb5d332f4375d2b67f33c724dd06a
Federico Negri <federico.negri@epfl.ch>
2016-04-22 12:51:51 +0200

    Updates here and there

--- RB_library/solve_HFsystem.m
+++ RB_library/solve_HFsystem.m
@@ -24,7 +24,22 @@ end
 
 uh                            = zeros(FOM.MESH.numNodes,1);
 uh(FOM.MESH.internal_dof)     = Ah \ Fh;
-uh(FOM.MESH.Dirichlet_dof)    = FOM.u_D(FOM.MESH.nodes(:,FOM.MESH.Dirichlet_dof), mu);
+
+% Apply BCs
+switch FOM.model
+    
+    case 'ADR'
+        
+        uh(FOM.MESH.Dirichlet_dof)    = FOM.u_D(FOM.MESH.nodes(:,FOM.MESH.Dirichlet_dof), mu);
+        
+    case 'CSM'
+                
+        for k = 1 : FOM.MESH.dim
+            
+            uh(FOM.MESH.Dirichlet_dof_c{k}+(k-1)*FOM.MESH.numNodes)    = FOM.u_D{k}(FOM.MESH.nodes(:,FOM.MESH.Dirichlet_dof_c{k}), mu);
+            
+        end
+end
 
 
 end
