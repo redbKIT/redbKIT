587073e179c45c35a9dce80fffdb1c527b6ee58a
Federico Negri <federico.negri@epfl.ch>
2016-06-15 19:04:50 +0200

    Speed up construction of P2 mesh in 2D

--- FEM_library/Mesh/P1toP2mesh2D.m
+++ FEM_library/Mesh/P1toP2mesh2D.m
@@ -1,21 +1,26 @@
 function [elements, vertices, boundaries] = P1toP2mesh2D(elements,vertices,boundaries)
 %P1TOP2MESH1D builds a P2 mesh in 2D.
 %       F. Saleri 9-20-01.
+%       F. Negri 2016, Add mesh Graph to speedup computations (still inefficient)
 
-[n,nov] = size(vertices);
-[n,noe] = size(elements);
+[~,nov]     =  size(vertices);
+[~,noe]     =  size(elements);
+nside       =  nov;
 
-nside    = nov;
-elements = [elements(1:3,:);zeros(3,noe); elements(4,:)];
+elements = [elements(1:3,:); zeros(3,noe); elements(4,:)];
 
-a = sparse(nov,nov);
+[ a ] = compute_adjacency(vertices, elements, 2, 'P1');
+
+[ii,jj,vv] = find(a);
+a = sparse(ii, jj, vv*0 - 1, nov, nov);
+
 for ie = 1:noe
     i = elements(1,ie);
     j = elements(2,ie);
     k = elements(3,ie);
     
     l1 = a(i,j);
-    if l1 == 0 
+    if l1 == -1 
        nside = nside + 1;
        a(i,j) = nside;
        a(j,i) = nside;
@@ -27,7 +32,7 @@ for ie = 1:noe
    end
    
    l2 = a(j,k);
-   if l2 == 0 
+   if l2 == -1 
        nside = nside + 1;
        a(j,k) = nside;
        a(k,j) = nside;
@@ -39,7 +44,7 @@ for ie = 1:noe
    end
    
    l3 = a(k,i);
-   if l3 == 0 
+   if l3 == -1 
        nside = nside + 1;
        a(k,i) = nside;
        a(i,k) = nside;
