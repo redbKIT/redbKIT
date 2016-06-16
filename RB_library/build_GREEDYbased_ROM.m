77f71939900030d5c878fe004cec90359355465c
Federico Negri <federico.negri@epfl.ch>
2016-06-16 17:33:23 +0200

    Small bugfix in default model name

--- RB_library/build_GREEDYbased_ROM.m
+++ RB_library/build_GREEDYbased_ROM.m
@@ -36,9 +36,9 @@ ROM.u_D    = FOM.u_D;
 
 if isfield(FOM, 'model')
     ROM.model   = FOM.model;
-    FOM.model   = 'ADR';
 else
     ROM.model = 'ADR';
+    FOM.model   = 'ADR';
 end
 
 ROM.MESH   = FOM.MESH;
