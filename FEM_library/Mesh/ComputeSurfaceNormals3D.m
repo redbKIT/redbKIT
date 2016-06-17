a33f6c96bdf3e63cddbbd823b3465a90c088f34d
Federico Negri <federico.negri@epfl.ch>
2016-06-17 10:09:18 +0200

    Fix warning message

--- FEM_library/Mesh/ComputeSurfaceNormals3D.m
+++ FEM_library/Mesh/ComputeSurfaceNormals3D.m
@@ -53,9 +53,9 @@ v2 = nodes(faces(:,3),1:3) - nodes(faces(:,1),1:3);
 % compute normals using cross product (nodes have same size)
 normals = normalizeVector3d(cross(v1, v2, 2));
 
-message = sprintf(['\nComputeSurfaceNormals3D assumes that boundary elements have correct orientation,\n', ...
-                  'so that surface normals are directed outward. No check on the sign is made.\n']);
-warning(message)
+message = sprintf(['\n\n[\bWarning]\b: ComputeSurfaceNormals3D assumes that boundary elements have correct orientation,\n', ...
+                  'so that surface normals are directed outward. No check on the sign is made.\n\n']);
+fprintf(message);
 
 %% check correct orientation (outward)
 % Uncomment the following lines if your mesh does not have outward normals
