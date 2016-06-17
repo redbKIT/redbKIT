c7395c76b70204f717630db5c142a98a187c9e50
Federico Negri <federico.negri@epfl.ch>
2016-06-17 07:43:29 +0200

    Update install file for metismex to support installation on Mac OS X

--- FEM_library/Mesh/install_metismex.m
+++ FEM_library/Mesh/install_metismex.m
@@ -52,18 +52,37 @@ end
 
 %% Run cmake
 
-!make config
-!make all
+%!make config
+%!make all
 
-%% download and install metismex
-fprintf('\nDownload metismex from git://github.com/dgleich/metismex.git (requires GIT!) ...')
-system('git clone git://github.com/dgleich/metismex.git');
-cd metismex
+%% Ask user to open a terminal and run cmake
+path_terminal = pwd;
+fprintf('\n*****************************************************\n');
+fprintf('ATTENTION: please open a terminal and type:\n%s\n',['cd ',path_terminal]);
+fprintf('\nThen run the following commands (requires CMAKE!):\n')
+fprintf('\nmake config\n')
+fprintf('make all\n')
 
-fprintf('\nCompile metismex ...')
-make
+reply2 = input('\nDid you succeed? Y/N:','s');
+if isempty(reply2)
+    reply2 = 'N';
+end
 
-fprintf('\nFinished!\n')
-cd ../../
-
+if strcmp(reply2,'Y')
+    
+    %% download and install metismex
+    fprintf('\nDownload metismex from git://github.com/dgleich/metismex.git (requires GIT!) ...')
+    system('git clone git://github.com/dgleich/metismex.git');
+    cd metismex
+    
+    fprintf('\nCompile metismex ...')
+    make
+    
+    fprintf('\nFinished!\n')
+    cd ../../
+else
+    cd ..
+    fprintf('\nAborted, try again.\n')
+    
+end
 end
