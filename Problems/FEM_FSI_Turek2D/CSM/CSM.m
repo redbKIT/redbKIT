3f2e5f196ab815782aa48bb1a069e88d323bed6e
Federico Negri <federico.negri@epfl.ch>
2016-06-21 19:28:36 +0200

    Add FSI Turek CSM benchmark set

---
+++ Problems/FEM_FSI_Turek2D/CSM/CSM.m
@@ -0,0 +1,67 @@
+clc
+clear all
+
+dim      =  2;
+fem      =  'P1';
+
+%% Load P1 mesh
+[vertices, boundaries, elements] = msh_to_Mmesh('Turek_mesh_Solid_Fine', dim);
+
+%% CSM1
+fprintf('\n------------ TEST CSM 1 --------------\n')
+
+[U, FE_SPACE, MESH, DATA] = CSM_Solver(dim, elements, vertices, boundaries, fem, 'CSM1_data', [], 'Figures/Sol_CSM1');
+
+% PostProcessing
+indexA = find(ismember(MESH.vertices(1:2,:)',[0.6 0.2],'rows'));
+
+fprintf('\nCSM1 RESULT:\nA-Point: x-Displacement = %1.3e m, y-Displacement = %1.3e m\n', U(indexA), U(indexA+FE_SPACE.numDofScalar))
+fprintf('\nREFERENCE VALUES:\nA-Point: x-Displacement = %1.3e m, y-Displacement = %1.3e m\n', -7.187*1e-3, -66.10*1e-3)
+
+
+%% CSM2
+fprintf('\n------------ TEST CSM 2 --------------\n')
+
+[U, FE_SPACE, MESH, DATA] = CSM_Solver(dim, elements, vertices, boundaries, fem, 'CSM2_data', [], 'Figures/Sol_CSM2');
+
+% PostProcessing
+indexA = find(ismember(MESH.vertices(1:2,:)',[0.6 0.2],'rows'));
+
+fprintf('\nCSM2 RESULT:\nA-Point: x-Displacement = %1.3e m, y-Displacement = %1.3e m\n', U(indexA), U(indexA+FE_SPACE.numDofScalar))
+fprintf('\nREFERENCE VALUES:\nA-Point: x-Displacement = %1.3e m, y-Displacement = %1.3e m\n', -0.4690*1e-3, -16.97*1e-3)
+
+
+%% CSM3
+fprintf('\n------------ TEST CSM 3 --------------\n')
+
+[U, FE_SPACE, MESH, DATA] = CSMt_Solver(dim, elements, vertices, boundaries, fem, 'CSM3_data');
+
+% PostProcessing
+indexA = find(ismember(MESH.vertices(1:2,:)',[0.6 0.2],'rows'));
+
+load ReferenceValues/csm3_l4_t0p005.point;
+
+t = DATA.time.t0:DATA.time.dt:DATA.time.tf;
+
+handle = figure;
+subplot(2,1,1)
+plot(t,U(indexA,1:length(t)))
+hold on
+plot(csm3_l4_t0p005(:,1),csm3_l4_t0p005(:,11),'--r')
+legend('Test', 'Reference value')
+xlabel('time [s]')
+ylabel('x-Displacement [m]')
+
+
+subplot(2,1,2)
+plot(t,U(indexA+FE_SPACE.numDofScalar,1:length(t)))
+hold on
+plot(csm3_l4_t0p005(:,1),csm3_l4_t0p005(:,12),'--r')
+legend('Test', 'Reference value')
+xlabel('time [s]')
+ylabel('y-Displacement [m]')
+hold on
+
+saveas(handle,'Figures/CSM3_Adisplacement','epsc');
+saveas(handle,'Figures/CSM3_Adisplacement','fig');
+
---
+++ Problems/FEM_FSI_Turek2D/CSM/CSM2_data.m
@@ -0,0 +1,36 @@
+%DATAFILE for CSM2 Test
+
+% Source term
+data.force{1} = @(x, y, t, param)(0.*x.*y);
+data.force{2} = @(x, y, t, param)(-10^3*2 + 0.*x.*y);
+
+% Dirichlet
+data.bcDir{1} = @(x, y, t, param)(0.*x.*y); 
+data.bcDir{2} = @(x, y, t, param)(0.*x.*y); 
+
+% Neumann
+data.bcNeu{1} = @(x, y, t, param)(0.*x.*y);
+data.bcNeu{2} = @(x, y, t, param)(0.*x.*y);
+
+% Normal Pressure
+data.bcPrex   = @(x, y, t, param)( 0.*x.*y);
+
+% BC flag
+data.flag_dirichlet{1}    =  [6];
+data.flag_neumann{1}      =  [7 8];
+data.flag_pressure{1}     =  [];
+
+data.flag_dirichlet{2}    =  [6];
+data.flag_neumann{2}      =  [7 8];
+data.flag_pressure{2}     =  [];
+
+% material parameters  
+data.Young   = 5.6*10^6;
+data.Poisson = 0.4;
+data.Density = 10^3;
+data.Material_Model   = 'StVenantKirchhoff';
+data.model   = 'CSM';
+
+% NonLinear Solver
+data.NonLinearSolver.tol               = 1e-8;
+data.NonLinearSolver.maxit             = 35;
