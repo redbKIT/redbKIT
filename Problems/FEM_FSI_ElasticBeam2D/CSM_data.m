507ff3fb7d92fe2a9917af2766ab0340fed4841c
Federico Negri <federico.negri@epfl.ch>
2016-07-13 18:21:40 +0200

    Update FSI Elastic Beam benchmark

--- Problems/FEM_FSI_ElasticBeam2D/CSM_data.m
+++ Problems/FEM_FSI_ElasticBeam2D/CSM_data.m
@@ -35,17 +35,8 @@ data.du0{2} = @(x, y, t, param)(0.*x.*y);
 data.Young   = 2.5*10^6;
 data.Poisson = 0.35;
 data.Density = 0.1;
-data.Material_Model   = 'StVenantKirchhoff';%'StVenantKirchhoff', Linear, NeoHookean2
+data.Material_Model   = 'StVenantKirchhoff';%'StVenantKirchhoff', Linear, % Attention: Neohookean not available in 2D
 data.model   = 'CSM';
-
-mu = data.Young / (2 + 2 * data.Poisson);
-lambda =  data.Young * data.Poisson /( (1+data.Poisson) * (1-2*data.Poisson) );
-bulk = ( 2.0 / 3.0 ) * mu + lambda;
-
-data.Alpha   = mu / 2;
-data.Beta    = 0.0;
-data.Bulk    = bulk;
-
 
 % NonLinear Solver
 data.NonLinearSolver.tol               = 1e-6;
